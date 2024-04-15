15d0622bb3c4b6b993b1035723a6450796a98655
EliPerman <145874379+EliPerman@users.noreply.github.com>
2024-04-15 12:59:33 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,8 +15,17 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    /* to be implemented in part (a) */
-    return 0;
+    int count = 0;
+    for (int i = 0; i < samples.length; i++){
+      if (samples[i] < -limit){
+        samples[i] = -limit;
+        count++;
+      } else if (samples[i] > limit){
+        samples[i] = limit;
+        count++;
+      }
+    }
+    return count;
   }
 
 
@@ -28,6 +37,12 @@ public class Sound
    */
   public void trimSilenceFromBeginning()
   {
-    /* to be implemented in part (b) */
+    int i = 0;
+    while (samples[i] == 0)
+      i++;
+    int[] arr = new int[samples.length - i];
+    for (int j = i; j < samples.length; i++)
+      arr[j - i] = samples[j];
+    samples = arr;
   }
 }
